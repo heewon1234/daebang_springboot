4b22e57f8350717998e946f6ede08d5bc80e0a94
MHW <a@b.com>
2023-12-04 09:21:42 +0900

    generic mapper // remove

--- src/main/java/com/kdt/mappers/GenericMapper.java
+++ src/main/java/com/kdt/mappers/GenericMapper.java
@@ -6,16 +6,10 @@ import org.mapstruct.BeanMapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-// generic에 들어오는건 dataType임
-// interface나 클래스 이름에 <> 해놓는건 generic ex> public class abc<> -> generic
-// new 할 떄 <>는 generic parameter ex> List<MembersDTO> list -> generic parameter
-
-
-//D : DTO / E : Entity ( 이름 바꿔도 됨 )
 
 public interface GenericMapper<D,E>{
 	
-	D toDto(E e); // Entity -> DTO
+	D toDto(E e);
 	E toEntity(D d);
 	
 	List<D> toDTOLIst(List<E> e);
