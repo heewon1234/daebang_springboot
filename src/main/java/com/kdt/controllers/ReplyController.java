120d0319836469f4267cbf686beb4bf40554fb00
MHW <a@b.com>
2023-12-07 15:15:28 +0900

    댓글 리스트 불러오기

--- src/main/java/com/kdt/controllers/ReplyController.java
+++ src/main/java/com/kdt/controllers/ReplyController.java
@@ -1,9 +1,13 @@
 package com.kdt.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +35,11 @@ public class ReplyController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> insertReply(@RequestBody ReplyDTO dto){
+	public ResponseEntity<List<ReplyDTO>> insertReply(@RequestBody ReplyDTO dto){
 		dto.setWriter(getUser().getUsername());
-		rServ.insertReply(dto);
-		return ResponseEntity.ok().build();
+		List<ReplyDTO> list = rServ.insertReply(dto);
+		return ResponseEntity.ok(list);
 	}
+	
 	
 }
--- src/main/java/com/kdt/repositories/ReplyRepository.java
+++ src/main/java/com/kdt/repositories/ReplyRepository.java
@@ -1,9 +1,13 @@
 package com.kdt.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kdt.domain.entities.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long>{
 
+	List<Reply> findAllByParentSeq(Long parentSeq);
+	
 }
