e4296c5c5e48705128e8cee9193544d698ac6444
DHJ <dahyeonee1324@naver.com>
2023-12-07 13:32:02 +0900

    signup update

--- src/main/java/com/kdt/services/MemberService.java
+++ src/main/java/com/kdt/services/MemberService.java
@@ -3,8 +3,10 @@ package com.kdt.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.kdt.config.SecurityConfig;
 import com.kdt.domain.entities.Member;
 import com.kdt.dto.MemberDTO;
 import com.kdt.mappers.MemberMapper;
@@ -18,8 +20,24 @@ public class MemberService {
 	@Autowired
 	private MemberMapper mMapper;
 	
+	private final PasswordEncoder passwordEncoder;
+
+    //@Autowired
+    public MemberService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+	
 	public List<MemberDTO> getAllMembers() {
 		List<Member> list = mRepo.findAll();
 		return mMapper.toDtoList(list);
+	}
+	
+	public void signUp(MemberDTO dto) {
+		dto.setRole("ROLE_MEMBER");
+		dto.setEnabled(true);
+		String hashedPassword = passwordEncoder.encode(dto.getPw());
+		dto.setPw(hashedPassword);
+		Member m = mMapper.toEntity(dto);
+		mRepo.save(m);
 	}
 }
