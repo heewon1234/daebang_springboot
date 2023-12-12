6b404e90f0ff366d98fe931d89f7c45373d287e0
KDH <kth5541915@gmail.com>
2023-12-12 15:19:08 +0900

    NG-104 <매물 삭제>

--- src/main/java/com/kdt/repositories/EstateOptionRepository.java
+++ src/main/java/com/kdt/repositories/EstateOptionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kdt.domain.entities.EstateOption;
 
 public interface EstateOptionRepository extends JpaRepository<EstateOption, Long>{
-
+	Long deleteByEstateCode(Long esateId);
 }
--- src/main/java/com/kdt/services/EstateService.java
+++ src/main/java/com/kdt/services/EstateService.java
@@ -1,7 +1,10 @@
 package com.kdt.services;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -9,20 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.kdt.domain.entities.UploadEstate;
+import com.kdt.domain.entities.Estate;
 import com.kdt.domain.entities.EstateImage;
 import com.kdt.domain.entities.EstateOption;
-import com.kdt.domain.entities.Estate;
-import com.kdt.dto.UploadEstateDTO;
-import com.kdt.dto.EstateOptionDTO;
+import com.kdt.domain.entities.UploadEstate;
 import com.kdt.dto.EstateDTO;
-import com.kdt.mappers.UploadEstateMapper;
-import com.kdt.mappers.EstateOptionMapper;
+import com.kdt.dto.EstateOptionDTO;
+import com.kdt.dto.UploadEstateDTO;
 import com.kdt.mappers.EstateMapper;
+import com.kdt.mappers.EstateOptionMapper;
+import com.kdt.mappers.UploadEstateMapper;
 import com.kdt.repositories.EstateImageRepository;
 import com.kdt.repositories.EstateOptionRepository;
-import com.kdt.repositories.UploadEstateRepository;
 import com.kdt.repositories.EstateRepository;
+import com.kdt.repositories.UploadEstateRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -46,19 +49,23 @@ public class EstateService {
 	private EstateRepository eRepo;
 
 	@Transactional
-	public void insertEstate(UploadEstateDTO dto, List<EstateOptionDTO> optionDTOList, List<MultipartFile> images) throws Exception {
+	public void insertEstate(UploadEstateDTO dto, List<EstateOptionDTO> optionDTOList, List<MultipartFile> images)
+			throws Exception {
 		// 매물 입력 ->
 		UploadEstate estate = ueMapper.toEntity(dto);
 
-		if(estate.getMaintenanceCost() == null) {
+		// 관리비 입력 안 했으면 0
+		if (estate.getMaintenanceCost() == null) {
 			estate.setMaintenanceCost(0L);
 		}
+		// 작성일 입력
 		estate.setWriteDate(new Timestamp(System.currentTimeMillis()));
+		
 		Long parentSeq = ueRepo.save(estate).getEstateId();
 		// <- 매물 입력
 
 		// 매물 옵션 입력 ->
-		for(EstateOptionDTO optionDTO : optionDTOList) {
+		for (EstateOptionDTO optionDTO : optionDTOList) {
 			EstateOption option = eoMapper.toEntity(optionDTO);
 			option.setEstateCode(parentSeq);
 
@@ -67,14 +74,16 @@ public class EstateService {
 		// <- 매물 옵션 입력
 
 		// 사진 파일 입력 ->
-		if(images.size() != 0) {
+		if (images.size() != 0) {
 			String upload = "c:/uploads/estateImages/";
 			File uploadPath = new File(upload);
-			if(!uploadPath.exists()) {uploadPath.mkdir();}
+			if (!uploadPath.exists()) {
+				uploadPath.mkdir();
+			}
 
-			for(MultipartFile image :images) {
+			for (MultipartFile image : images) {
 				String oriName = image.getOriginalFilename();
-				String sysName = UUID.randomUUID()+"_"+oriName;
+				String sysName = UUID.randomUUID() + "_" + oriName;
 
 				image.transferTo(new File(uploadPath, sysName));
 				eiRepo.save(new EstateImage(null, oriName, sysName, parentSeq));
@@ -82,23 +91,58 @@ public class EstateService {
 		}
 		// <- 사진 파일 입력
 	}
-	
+
 	public List<EstateDTO> selectAll() {
-		List<Estate> eList =  eRepo.findAll();
+		List<Estate> eList = eRepo.findAll();
 		List<EstateDTO> list = eMapper.toDtoList(eList);
-		
+
 		return list;
 	}
-	
-//	@Transactional
-//	public void deleteById(Long estateId) {
-//		// 매물 정보 삭제
-//		eRepo.deleteById(estateId);
-//		// 매물 옵션 정보 삭제
-//		eoRepo.deleteByEstateCode(estateId);
-//		// 사진 파일 삭제
-//		eiRepo.deledeleteByParentId(estateId);
-//		 
-//		return;
-//	}
+
+	@Transactional
+	public void deleteById(Long estateId) throws Exception {
+		// 매물 옵션 정보 삭제
+		eoRepo.deleteByEstateCode(estateId);
+		
+		// 사진 파일 삭제 ->
+		// 실제로 지울 파일 이름 검색
+		List<EstateImage> eiList = eiRepo.findSysNamesByParentId(estateId);
+		List<String> delFileList = new ArrayList<>();
+		for (EstateImage image : eiList) {
+			delFileList.add(image.getSysName());
+		}
+		delServerFile(delFileList);
+		// DB에서 삭제
+		eiRepo.deleteByParentId(estateId);
+		// <- 사진 파일 삭제
+
+		// 매물 정보 삭제
+		eRepo.deleteById(estateId);
+
+		return;
+	}
+
+	// 사진 파일 삭제
+	public void delServerFile(List<String> delFileList) throws Exception {
+		String filePath = "C:/uploads";
+		File uploadFilePath = new File(filePath);
+		if (!uploadFilePath.exists()) {
+			uploadFilePath.mkdir();
+		}
+
+		String realPath = "C:/uploads/estateImages/";
+		File uploadPath = new File(realPath);
+		if (!uploadPath.exists()) {
+			uploadPath.mkdir();
+		}
+
+		if (delFileList != null) {
+			for (String delFile : delFileList) {
+				if (delFile != null) {
+					Path path = Paths.get(uploadPath + "/" + delFile);
+					java.nio.file.Files.deleteIfExists(path);
+				}
+			}
+		}
+	}
 }
