c6ba29541455dcf0837fdbd994b1d307e2b7ea1a
DHJ <dahyeonee1324@naver.com>
2023-12-15 15:34:04 +0900

    estate Info update

--- src/main/java/com/kdt/controllers/EstateAgentController.java
+++ src/main/java/com/kdt/controllers/EstateAgentController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kdt.dto.RealEstateAgentDTO;
+import com.kdt.dto.UpdateEstateDTO;
 import com.kdt.services.AgentService;
 
 @RestController
@@ -42,6 +44,12 @@ public class EstateAgentController {
 	@DeleteMapping("delete/{id}")
 	public ResponseEntity<Void> deleteById(@PathVariable String id){
 		aServ.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PostMapping("updateMyInfo")
+	public ResponseEntity<Void> updateMyInfo(@RequestBody UpdateEstateDTO dto) {
+		aServ.updateMyInfo(dto);
 		return ResponseEntity.ok().build();
 	}
 }
