dbc56d85892301fb67e3cab4eb779ca5c0772435
KDH <kth5541915@gmail.com>
2023-12-15 16:32:26 +0900

    NG-114 <매물 수정 기능>

--- src/main/java/com/kdt/repositories/EstateRepository.java
+++ src/main/java/com/kdt/repositories/EstateRepository.java
@@ -10,5 +10,5 @@ import com.kdt.domain.entities.Estate;
 public interface EstateRepository extends JpaRepository<Estate, Long> {
 	@EntityGraph(attributePaths = { "building", "heatingSystem", "room", "structure", "transaction", "images",
 			"optionList" })
-	List<Estate> findAll();
+	List<Estate> findAllByWriter(String loginId);
 }
