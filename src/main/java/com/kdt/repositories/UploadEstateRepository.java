c681bf34354a8dddfcf43ee0f9ce0bc9a28fa703
KDH <kth5541915@gmail.com>
2023-12-17 17:37:57 +0900

    NG-124 <문의하기 버튼 클릭 이벤트>

--- src/main/java/com/kdt/repositories/UploadEstateRepository.java
+++ src/main/java/com/kdt/repositories/UploadEstateRepository.java
@@ -1,8 +1,11 @@
 package com.kdt.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kdt.domain.entities.UploadEstate;
 
 public interface UploadEstateRepository extends JpaRepository<UploadEstate, Long>{
+	 List<UploadEstate> findByWriter(String loginId);
 }
