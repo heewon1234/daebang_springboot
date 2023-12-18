03fc71c05885814b8854c2d5d2b8da691c7b2f7e
KDH <kth5541915@gmail.com>
2023-12-18 10:12:10 +0900

    NG-124 <리뷰 권한 업데이트>

--- src/main/java/com/kdt/repositories/ReviewApprovalRepository.java
+++ src/main/java/com/kdt/repositories/ReviewApprovalRepository.java
@@ -3,14 +3,19 @@ package com.kdt.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.kdt.domain.entities.ReviewApproval;
-import com.kdt.dto.ReviewApprovalDTO;
 
 public interface ReviewApprovalRepository extends JpaRepository<ReviewApproval, Long>{
 
 	List<ReviewApproval> findAllByUserId(String loginId);
 	
 	ReviewApproval findByApprovalCodeAndEstateCode(String approvalCode, Long estateCode);
+	
+	@Modifying
+	@Query("UPDATE ReviewApproval ra SET ra.approvalCode = :approvalCode WHERE ra.seq = :seq")
+	void updateStatue(Long seq, String approvalCode);
+	
 }
