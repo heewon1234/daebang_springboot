bda2dbb44d1969c5384702b85105162715332bba
KDH <kth5541915@gmail.com>
2023-12-26 01:17:21 +0900

    NG-89 <매물 판매 상황 변경>

--- src/main/java/com/kdt/services/EstateService.java
+++ src/main/java/com/kdt/services/EstateService.java
@@ -118,7 +118,7 @@ public class EstateService {
 	}
 
 	public List<EstateDTO> selectAll() {
-		List<Estate> eList = eRepo.findAll();
+		List<Estate> eList = eRepo.findAllBySoldStatusFalse();
 		List<EstateDTO> list = eMapper.toDtoList(eList);
 
 		for(EstateDTO dto : list) {
@@ -324,5 +324,14 @@ public class EstateService {
 		edto.getRealEstateAgent().setPw(null);
 		
 		return edto;
+	}
+	
+	public void updateStatus(Long estateId) {
+		Estate estate = eRepo.findById(estateId).get();
+		estate.setSoldStatus(!estate.isSoldStatus());
+		
+		System.out.println(estate);
+		
+		eRepo.save(estate);
 	}
 }
