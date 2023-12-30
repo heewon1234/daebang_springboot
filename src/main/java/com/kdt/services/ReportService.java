cc1f778cb1428d6df63f3851855465909a77d701
가입dldidsla13 <dldidsla13@naver.com>
2023-12-30 20:58:14 +0900

    수정

--- src/main/java/com/kdt/services/ReportService.java
+++ src/main/java/com/kdt/services/ReportService.java
@@ -6,17 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kdt.domain.entities.MyReport;
+import com.kdt.domain.entities.RealEstateAgent;
 import com.kdt.domain.entities.Report;
 import com.kdt.domain.entities.ReportStatus;
 import com.kdt.dto.MyReportDTO;
 import com.kdt.dto.ReportDTO;
 import com.kdt.mappers.MyReportMapper;
 import com.kdt.mappers.ReportMapper;
+import com.kdt.repositories.AgentRepository;
 import com.kdt.repositories.MyReportRepository;
 import com.kdt.repositories.ReportRepository;
 
+import jakarta.transaction.Transactional;
+
+@Transactional
 @Service
 public class ReportService {
+	@Autowired
+	private AgentRepository aRepo;
+	
 	@Autowired
 	private ReportRepository rRepo;
 	
@@ -52,21 +60,76 @@ public class ReportService {
         List<ReportDTO> dtos = rMapper.toDtoList(list);
         return dtos;
     }
+//	public void approve(Long seq) {
+//		Report e = rRepo.findBySeq(seq);
+//		ReportStatus reportStatus = new ReportStatus();
+//		reportStatus.setId("rs2");
+//		e.setReportStatus(reportStatus);
+//		rRepo.save(e);
+//		
+//		 // RealEstateAgent의 report_Count 증가 로직
+//	    RealEstateAgent agent = e.getRealEstateAgent();
+//	    agent.setReport_Count(agent.getReport_Count() + 1);
+//	    aRepo.save(agent);
+//	}
 	public void approve(Long seq) {
-		Report e = rRepo.findBySeq(seq);
-		ReportStatus reportStatus = new ReportStatus();
-		reportStatus.setId("rs2");
-		e.setReportStatus(reportStatus);
+	    Report e = rRepo.findBySeq(seq);
+	    ReportStatus reportStatus = new ReportStatus();
+	    reportStatus.setId("rs2");
+	    e.setReportStatus(reportStatus);
+	    rRepo.save(e);
 
-		rRepo.save(e);
+	    // RealEstateAgent의 report_Count 증가 로직
+	    RealEstateAgent agent = e.getRealEstateAgent();
+	    Long reportCount = agent.getReport_Count();
+
+	    // 5의 배수인 경우 매너 온도 조정
+	    if ((reportCount + 1) % 5 == 0) {
+	        int deductionCount = (int) ((reportCount + 1) / 5); // 5의 배수 횟수 계산
+	        double mannerTemperatureDelta = 0.5; // 감소량
+
+	        // 매너 온도를 조정하는 로직
+	        double updatedMannerTemperature = agent.getManners_temperature() - (mannerTemperatureDelta);
+	        agent.setManners_temperature(updatedMannerTemperature);
+	    }
+
+	    // report_Count 증가
+	    agent.setReport_Count(reportCount + 1);
+	    aRepo.save(agent);
 	}
+
+
+
+
+
+
+
 	public void revoke_approval(Long seq) {
-		Report e = rRepo.findBySeq(seq);
-		ReportStatus reportStatus = new ReportStatus();
-		reportStatus.setId("rs1");
-		e.setReportStatus(reportStatus);
-		rRepo.save(e);
+	    Report e = rRepo.findBySeq(seq);
+	    ReportStatus reportStatus = new ReportStatus();
+	    reportStatus.setId("rs1");
+	    e.setReportStatus(reportStatus);
+	    rRepo.save(e);
+	    
+	    // RealEstateAgent의 report_Count 감소 로직
+	    RealEstateAgent agent = e.getRealEstateAgent();
+	    Long reportCount = agent.getReport_Count();
+
+	    if (reportCount > 0) {
+	        agent.setReport_Count(reportCount - 1);
+	        aRepo.save(agent);
+	        
+	        // 감소된 report_Count가 5의 배수일 때, manner_temperature 복구 로직
+	        if (reportCount % 5 == 0) {
+	            int deductionCount = (int) (reportCount / 5); // 5의 배수 횟수 계산
+	            double mannerTemperatureDelta = 0.5; // 감소량
+	            double updatedMannerTemperature = agent.getManners_temperature() + (mannerTemperatureDelta * deductionCount);
+	            agent.setManners_temperature(updatedMannerTemperature);
+	            aRepo.save(agent);
+	        }
+	    }
 	}
+
 	
 	public void reject(Long seq) {
 		Report e = rRepo.findBySeq(seq);
