f99c61f65444b4d2dd538f36edcd9b52543db8c2
s2zz <hey_spring@naver.com>
2023-12-31 00:58:43 +0900

    report update

--- src/main/java/com/kdt/domain/entities/ReportStatus.java
+++ src/main/java/com/kdt/domain/entities/ReportStatus.java
@@ -2,16 +2,14 @@ package com.kdt.domain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Report_Status")
 public class ReportStatus {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private String id;
 
