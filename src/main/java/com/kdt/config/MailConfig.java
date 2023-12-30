a08985820c97f8c20c2f6ff90f894e52bbaec1dd
SJL <zun0352@naver.com>
2023-12-30 20:09:29 +0900

    estate repository 쿼리 수정

--- src/main/java/com/kdt/config/MailConfig.java
+++ src/main/java/com/kdt/config/MailConfig.java
@@ -18,8 +18,8 @@ public class MailConfig {
     protected JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
-        javaMailSender.setUsername("daebbang741"); // 네이버 아이디
-        javaMailSender.setPassword("daebbang123"); // 네이버 비밀번호
+        javaMailSender.setUsername("a"); // 네이버 아이디
+        javaMailSender.setPassword("a"); // 네이버 비밀번호
         
 
         javaMailSender.setPort(465 ); // 메일 인증서버 포트
